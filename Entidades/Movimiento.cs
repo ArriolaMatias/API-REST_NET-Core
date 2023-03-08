41adaf7c948a860b9b0a0dac5b56974439796448
ArriolaMatias <arriolamattias@gmail.com>
2023-03-08 01:06:35 -0300

    Comentadas las subcategorias hasta su implementacion.

--- Entidades/Movimiento.cs
+++ Entidades/Movimiento.cs
@@ -9,11 +9,14 @@
         public DateTime Fecha { get; set; }
         public string? Detalle { get; set; }
         public Categoria? Categoria { get; set; }
-        public Subcategoria? Subcategoria { get; set; }
+        public int? CategoriaId { get; set; }
+        //public Subcategoria? Subcategoria { get; set; }
+        //public int SubcategoriaID { get; set; }
         public int? IdAsociado { get; set; }
         public Tipo TipoDeMovimiento { get; set; }
         
         public enum Tipo
+
         {
             _INGRESO,
             EGRESO
