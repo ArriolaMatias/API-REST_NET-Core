af2f769ece40e47148b1870b9f0a7f2e12532b3d
ArriolaMatias <arriolamattias@gmail.com>
2023-03-08 01:30:47 -0300

    Implementados metodos Create y Get en la capa de negocios.

---
+++ API/Business Layer/BlMovimiento.cs
@@ -0,0 +1,24 @@
+﻿using DataAccess.Repository;
+using Entities;
+
+namespace API.Business_Layer
+{
+    public class BlMovimiento
+    {
+        private readonly IRepository<Movimiento> _repository;
+
+        public BlMovimiento(IRepository<Movimiento> repository)
+        {
+            this._repository = repository;
+        }
+
+        public async Task<csResponse> GetMovimientos()
+        {
+            return await _repository.Read();
+        }
+        public async Task<csResponse> Create(Movimiento movimiento)
+        {
+            return await _repository.Create(movimiento);
+        }
+    }
+}
