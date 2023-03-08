18ec4b9d91ca2fe01d835ec8fbbecbe3d256152d
ArriolaMatias <arriolamattias@gmail.com>
2023-03-08 01:31:04 -0300

    Implementado controller para manejar los movimientos

---
+++ API/Controllers/MovimientoController.cs
@@ -0,0 +1,33 @@
+﻿using API.Business_Layer;
+using DataAccess.Repository;
+using Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[Action]")]
+    public class MovimientoController : ControllerBase
+    {
+        private readonly IRepository<Movimiento> _repositoryMovimiento;
+        private BlMovimiento _blMovimiento;
+        public MovimientoController(IRepository<Movimiento> repository)
+        {
+            _repositoryMovimiento = repository;
+            _blMovimiento = new BlMovimiento(_repositoryMovimiento);
+        }
+
+        [HttpGet]
+        public async Task<csResponse> GetMovimientos()
+        {
+            return await _blMovimiento.GetMovimientos();
+        }
+
+        [HttpPost]
+        public async Task<csResponse> AddMovimiento(Movimiento movimiento)
+        {
+            return await _blMovimiento.Create(movimiento);
+        }
+    }
+}
