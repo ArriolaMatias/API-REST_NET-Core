15fa77a734876ae827f68f1d9f0ef493792238b2
ArriolaMatias <arriolamattias@gmail.com>
2023-10-24 21:52:56 -0300

    Agregada clase usuario para utilizar JWT

---
+++ Entidades/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
