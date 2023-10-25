82859e8bafb10d71cb914d8198c2db1fcdf74c3f
ArriolaMatias <arriolamattias@gmail.com>
2023-10-24 21:53:46 -0300

    Implementado servicio de JWT

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
 using DataAccess.EntityFramework;
 using DataAccess.Repository;
 using Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Text.Json.Serialization;
 
 namespace API
@@ -13,6 +16,29 @@ namespace API
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Configuration.AddJsonFile("appsettings.json");
+            var secretKey = builder.Configuration.GetSection("JWTsettings").GetSection("secretKey").ToString();
+            var keyBytes = Encoding.UTF8.GetBytes(secretKey);
+
+            builder.Services.AddAuthentication(config =>
+            {
+                config.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                config.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(config => 
+                {
+                config.RequireHttpsMetadata = false;
+                config.SaveToken = true;
+                config.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(keyBytes),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+
+                };
+            });
+            
 
             builder.Services.AddControllers();
             builder.Services.AddControllers().AddJsonOptions(x =>
@@ -36,6 +62,8 @@ namespace API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
